cf0f3d25527a3f23f35db47757a037a7b9fdca2b
Grant Gapinski <gruntygap@gmail.com>
2019-07-24 02:19:57 -0500

    Another Mistake, this is why you shouldnt code at 2am kids

--- app/relay.py
+++ app/relay.py
@@ -1,25 +1,24 @@
 # Import required Python libraries
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
 pinList = [2]
 
 def initialize():
     # Use BCM GPIO references instead of physical pin numbers
-    #GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)
 
     # init list with pin numbers
     # loop through pins and set mode and state to 'low'
     for i in pinList:
-        print(i)
-        #GPIO.setup(i, GPIO.OUT)
-        #GPIO.output(i, GPIO.HIGH)
+        GPIO.setup(i, GPIO.OUT)
+        GPIO.output(i, GPIO.HIGH)
 
 def trigger() :
         for i in pinList:
-          #GPIO.output(i, GPIO.LOW)
+          GPIO.output(i, GPIO.LOW)
           time.sleep(0.25)
-          #GPIO.output(i, GPIO.HIGH)
+          GPIO.output(i, GPIO.HIGH)
           time.sleep(0.25)
-          #GPIO.cleanup()
+          GPIO.cleanup()
 
